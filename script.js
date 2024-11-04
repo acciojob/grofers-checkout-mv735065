a5b170cd5b7de82ab2fe4f7d99b0a5c841c08b65
Vamshi Reddy <106398124+mv735065@users.noreply.github.com>
2024-11-04 19:22:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,24 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	let prices= document.querySelectorAll(".price");
+	let sum=0;
+	for(let price of prices) {
+		let val = parseFloat(price.innerText);
+		sum+=val;
+	}
+    if(document.querySelector(".newRow")){
+		document.querySelector(".newRow").remove();
+	}
+	
+	let row=document.createElement('tr');
+	row.className="newRow";
+		let tdd=document.createElement('td');
+	tdd.innerText=sum;
+	row.append(tdd);
+	
+   document.querySelector("table").append(row);
+	
 };
 
 getSumBtn.addEventListener("click", getSum);
